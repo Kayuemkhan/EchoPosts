fa00bde33959c17557baffaa6ce85d09012f9904
Kayuemkhan <abdulkayuem007@gmail.com>
2025-08-23 16:57:31 +0600

    initiliazed project , initilization setup for project

---
+++ app/src/main/java/com/example/echoposts/ui/favourites/FavouritesFragment.kt
@@ -0,0 +1,40 @@
+package com.example.echoposts.ui.favourites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.echoposts.databinding.FragmentNotificationsBinding
+
+class FavouritesFragment : Fragment() {
+
+    private var _binding: FragmentNotificationsBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val favouritesViewModel =
+            ViewModelProvider(this).get(FavouritesViewModel::class.java)
+
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textNotifications
+        favouritesViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
