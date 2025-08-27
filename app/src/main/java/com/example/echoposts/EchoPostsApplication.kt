efe20d24e3dc9964c767de6513dde027b9009317
abdulkayuem <abdulkayuem007@gmail.com>
2025-08-27 22:42:49 +0600

    Update EchoApplication to support dark theme

--- app/src/main/java/com/example/echoposts/EchoPostsApplication.kt
+++ app/src/main/java/com/example/echoposts/EchoPostsApplication.kt
@@ -1,7 +1,22 @@
 package com.example.echoposts
 
+
+
 import android.app.Application
+import com.example.echoposts.data.ThemeManager
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class EchoPostsApplication : Application()
+class EchoPostsApplication : Application() {
+
+    @Inject
+    lateinit var themeManager: ThemeManager
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Apply saved theme preference
+        themeManager.applyTheme()
+    }
+}
